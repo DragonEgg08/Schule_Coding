087d1be17d32cdf9c33a03b9a63dabeb47c94601
Elias <peanut110308@gmail.com>
2025-07-08 19:06:26 +0200

    f

--- Buch Game/Regeln_Werkzeuge.py
+++ Buch Game/Regeln_Werkzeuge.py
@@ -1,12 +1,12 @@
 import random
 
-class Würfel():
+class Würfel:
     def __init__(self, anzahlSeiten:int=20):
             self.anzahlSeiten = anzahlSeiten
     def würfeln(self) -> int:
         return random.randint(1, self.anzahlSeiten)
 
-class Kampfregeln():
+class Kampfregeln:
     def __init__(self):
         pass
     def kampf(self, Held, Gegner):
