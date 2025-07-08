6862682772c20cde1cbdd5a3eaed3bdac4dd2bf5
elias <peanut110308@gmail.com>
2025-07-08 15:50:14 +0200

    f

--- Buch Game/Helden.py
+++ Buch Game/Helden.py
@@ -1,16 +1,17 @@
 class Held():
-    def __init__(self, stärke:int, angriffswert:int, lebenspunkte:int ,name:int="Held"):
+    def __init__(self, stärke:int, angriffswert:int, lebenspunkte:int ,name:str="Held"):
         self.name = name
         self.stärke = stärke
 
+
 class Zauberer(Held):
-    def __init__(self, name: str = "Merlin"):
-        super().__init__(name, )
+    def __init__(self, stärke:int, angriffswert:int, lebenspunkte:int, name: str = "Merlin"):
+        super().__init__(name, stärke, angriffswert, lebenspunkte)
 
     def heilen(self):
-
+        pass
 
 
 class Krieger(Held):
-    def __init__(self, name: str = "Link"):
-        super().__init__(name, )
+    def __init__(self, stärke:int, angriffswert:int, lebenspunkte:int, name: str = "Link"):
+        super().__init__(name, stärke, angriffswert, lebenspunkte)
